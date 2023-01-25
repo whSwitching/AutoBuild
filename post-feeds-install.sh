8226dcc01abf3a19467aeeddc13254298843a135
whSwitching <ohmymymy@qq.com>
2023-01-25 17:01:21 +0800

    Update post-feeds-install.sh

--- post-feeds-install.sh
+++ post-feeds-install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 #
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
@@ -9,6 +9,6 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
+echo "post feeds install script run"
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
