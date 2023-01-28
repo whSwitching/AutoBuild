244413d99b3dfa311dd716997742d0aeca6b0bab
whSwitching <ohmymymy@qq.com>
2023-01-28 19:38:00 +0800

    Create pre-feeds-install.sh

---
+++ OpenWrt/pre-feeds-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "pre feeds install script run"
+
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
